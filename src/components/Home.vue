fd34df8581b7fa6eaa231eef57a92a15d8d4e431
mateuszmu <unnamed109@wp.pl>
2025-10-07 16:30:52 +0200

    Przepisany pierwszy komponent w zakresie template- Home jako strona startowa.

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="home">
+    <div>
+      <h1>Welcome to the Star Wars Data Explorer!</h1>
+      <p class="description">
+        Discover characters, planets, and species from the Star Wars universe
+        using the SWAPI Reborn API. Click any section from the menu above to get
+        started.
+      </p>
+    </div>
+    <figure>
+      <blockquote class="blockquote">
+        <p>Do. Or do not. There is no try. </p>
+      </blockquote>
+      <figcaption class="blockquote-footer">
+        Yoda
+        <cite title="Source Title">STAR WARS: THE EMPIRE STRIKES BACK</cite>
+      </figcaption>
+    </figure>
+  </div>
+</template>
